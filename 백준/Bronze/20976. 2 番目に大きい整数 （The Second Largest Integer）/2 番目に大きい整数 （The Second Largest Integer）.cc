7fde30d1587463c11b894c31b927567249b74908
Cheongpark(청박) <55844749+cheongpark@users.noreply.github.com>
2022-10-02 16:04:13 +0900

    [Bronze IV] Title: 2 番目に大きい整数 (The Second Largest Integer), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20976. 2 番目に大きい整数 （The Second Largest Integer）/2 番目に大きい整数 （The Second Largest Integer）.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int N[3] = {};
+	
+	for (int i = 0; i < 3; i++)
+		cin >> N[i];
+	
+	sort(N, N + 3);
+
+	cout << N[1];
+}
