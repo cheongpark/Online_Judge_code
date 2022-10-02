3fc29bae6fa5e56f1ef1bcf10cf00ec8dd23f6db
Cheongpark(청박) <55844749+cheongpark@users.noreply.github.com>
2022-10-02 15:50:18 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int N = 0, count = 0;
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		string word = "";
+		bool check[26] = { false, };
+		cin >> word;
+
+		check[(int)word[0] - 97] = true;
+
+		if (word.length() == 1)
+			count++;
+
+		for (int j = 1; j < word.length(); j++) {
+			if (word[j] != word[j - 1]) {
+				if (check[(int)word[j] - 97])
+					break;
+				check[(int)word[j] - 97] = true;
+			}
+			if (j == word.length() - 1)
+				count++;
+			//cout << word[i] << " " << word[i + 1] << endl;
+		}
+	}
+	cout << count;
+}
